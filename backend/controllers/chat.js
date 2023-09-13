23b6ea51e5be9090a33a1f00d48ec413665730d0
Yashraj Shukla <yashrajshukla48@gmail.com>
2023-09-13 18:41:01 +0530

    Customer raising query controller in chat component

--- backend/controllers/chat.js
+++ backend/controllers/chat.js
@@ -57,3 +57,31 @@ exports.fetchChatList = async (req, res) => {
     throw new Error(error.message);
   }
 };
+
+exports.raiseQuery = async (req, res) => {
+  try {
+    const { chatId, userId } = req.body;
+    const exists = await Chat.findOne({ chatId: chatId });
+    if (exists) {
+      const chat = await Chat.findOne({ _id: exists._id }).populate("users");
+      return res.status(200).json({
+        success: true,
+        message: "chat already initialized!",
+        fullChat: chat,
+      });
+    }
+    const newChat = await Chat.create({
+      chatId: chatId,
+      users: [userId],
+    });
+
+    const fullChat = await Chat.findOne({ _id: newChat._id }).populate("users");
+
+    return res.status(200).json({
+      success: true,
+      fullChat: fullChat,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
